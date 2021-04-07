8695dac7f8d154deed847657c7141c724a17aec1
ismayton <ian.mayton@gmail.com>
2021-04-07 14:53:37 -0500

    style for lesson content and videos

--- src/components/Lesson.js
+++ src/components/Lesson.js
@@ -6,7 +6,7 @@ export default class Lesson extends Component {
         return <div className="lesson">
             <h3>{this.props.lesson.title}</h3>
             <p className="content">{this.props.lesson.content}</p>
-            <iframe src={this.props.lesson.video_url} title={this.props.lesson.title}></iframe>
+            <iframe className="video" src={this.props.lesson.video_url} title={this.props.lesson.title}></iframe>
         </div>
     }
 }
