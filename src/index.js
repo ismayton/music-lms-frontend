c4eb2253b96e5e5dcfe851c096979af82fb00de2
ismayton <ian.mayton@gmail.com>
2021-05-11 11:09:16 -0500

    added .htaccess to build file, serves index.html with client side routing

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import thunk from 'redux-thunk';
 import App from './App';
 import rootReducer from './reducers/rootReducer'
 
+// import express from 'express';
+// import ReactDOMServer from 'react-dom/server';
+// import { StaticRouter } from 'react-router-dom';
+
 const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.hydrate(
@@ -15,3 +19,28 @@ ReactDOM.hydrate(
   </Provider>,
   document.getElementById('root')
 );
+
+// app.get('/*', (req, res) => {
+//   const context = {};
+//   const app = ReactDOMServer.renderToString(
+//     <StaticRouter location={req.url} context={context}>
+//       <App />
+//     </StaticRouter>
+//   );
+
+//   const indexFile = path.resolve('./build/index.html');
+//   fs.readFile(indexFile, 'utf8', (err, data) => {
+//     if (err) {
+//       console.error('Something went wrong:', err);
+//       return res.status(500).send('Oops, better luck next time!');
+//     }
+
+//     return res.send(
+//       data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+//     );
+//   });
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`😎 Server is listening on port ${PORT}`);
+// });
